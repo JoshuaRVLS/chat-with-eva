b8145fcf0304180b30d1c31821ea680f23b506ed
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-20 22:23:41 +0700

    rework

--- app/stores/useName.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useName = create<{
-  name: string;
-  setName: (name: string) => void;
-}>((set) => ({
-  name: "",
-  setName: (name: string) => set({ name }),
-}));
