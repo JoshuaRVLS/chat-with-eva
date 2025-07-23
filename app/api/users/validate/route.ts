4dbd0e01214962e1c1a2c155b6cc3f9a22b78f94
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-23 22:53:18 +0700

    Validate credentials for login

---
+++ app/api/users/validate/route.ts
@@ -0,0 +1,59 @@
+import { db } from "@/app/utils/prisma";
+import { z } from "zod";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+const userSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+export const POST = async (req: Request) => {
+  try {
+    const { username, password } = await userSchema.parseAsync(
+      await req.json()
+    );
+    const user = await db.user.findFirst({
+      where: {
+        OR: [{ username }, { email: username }],
+      },
+    });
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: "User belum terdaftar" },
+        { status: 401 }
+      );
+    }
+    if (!(await bcrypt.compare(password, user.password))) {
+      return NextResponse.json(
+        { success: false, message: "Password salah" },
+        { status: 401 }
+      );
+    }
+
+    if (!user.verified) {
+      return NextResponse.json(
+        {
+          success: true,
+          message: "Tolong aktivasi akun terlebih dahulu",
+          userId: user.id,
+        },
+        {
+          status: 200,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, message: "Login berhasil", userId: user.id },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ error: error.issues }, { status: 400 });
+    }
+  }
+};
