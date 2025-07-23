fd10c27d74b782788b3ae979f7ff73a729ad3624
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-23 23:37:30 +0700

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -27,5 +27,9 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/login", "/register"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/login",
+    "/register",
+  ],
 };
