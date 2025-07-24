af2bd7fe1c40b2130c15d89a35d8c74e77c5f905
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-24 12:44:04 +0700

    Profile

---
+++ app/components/Navbar/Profile.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { AuthContext } from "@/app/providers/AuthProvider";
+import Image from "next/image";
+import React, { useContext, useEffect, useState } from "react";
+import Reveal from "../Animations/Reveal";
+import { signOut } from "next-auth/react";
+
+const Profile = () => {
+  const { user } = useContext(AuthContext);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if ((event.target as HTMLElement).id !== "profile") {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    if (menuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [menuOpen]);
+
+  return (
+    <div>
+      <Image
+        onClick={() => setMenuOpen(!menuOpen)}
+        src={`/api/users/picture/${user?.id}`}
+        alt={`Your Profile`}
+        width={40}
+        height={40}
+        className="object-cover rounded-full"
+      />
+
+      {menuOpen && (
+        <Reveal direction="right">
+          <div className="relative -z-10 select-none">
+            <div className="absolute flex gap-2 min-w-36 top-2 right-0 w-fit p-4 border-borders bg-primary-background border font-light">
+              <button className="btn-outline w-full" onClick={() => signOut()}>
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </Reveal>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
