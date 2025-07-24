c4f0d8f01790ec75ef7d2bc087d921083ae5093b
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-24 12:43:49 +0700

    Added direction

--- app/components/Animations/Reveal.tsx
+++ app/components/Animations/Reveal.tsx
@@ -6,13 +6,23 @@ interface RevealProps {
 
   delay?: number;
   duration?: number;
+  direction?: "down" | "up" | "left" | "right";
 }
 
-const Reveal = ({ children, delay = 0, duration = 0.5 }: RevealProps) => {
+const Reveal = ({
+  children,
+  delay = 0,
+  duration = 0.5,
+  direction = "up",
+}: RevealProps) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
+      initial={{
+        opacity: 0,
+        y: direction === "down" ? 100 : direction === "up" ? -100 : 0,
+        x: direction === "right" ? 100 : direction === "left" ? -100 : 0,
+      }}
+      animate={{ opacity: 1, y: 0, x: 0 }}
       transition={{
         duration: duration,
         delay: delay,
