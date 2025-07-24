b5d809718f6d0ac1c8fbadbacada99f563351fc8
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-24 12:42:45 +0700

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div></div>;
 };
 
 export default HomePage;
