3bcc0767ddf8d16d009ff43e4cfcaee6c20c410c
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-24 12:43:55 +0700

    Menu for logged in user

---
+++ app/components/Navbar/AuthenticatedMenu.tsx
@@ -0,0 +1,17 @@
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+import Profile from "./Profile";
+
+const AuthenticatedMenu = () => {
+  return (
+    <div className="flex gap-2 items-center">
+      <Link className="rounded-full btn-outline" href={"/"}>
+        Create Character
+      </Link>
+      <Profile />
+    </div>
+  );
+};
+
+export default AuthenticatedMenu;
