0d3f772f43c9a5f03e9f863eabba505727e7ea69
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-24 12:43:09 +0700

    Get profile image by user id

---
+++ app/api/users/picture/[userId]/route.ts
@@ -0,0 +1,30 @@
+// app/api/users/profile-image/[userId]/route.ts
+import { db } from "@/app/utils/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { userId: string } }
+) {
+  try {
+    const user = await db.user.findUnique({
+      where: { id: params.userId },
+      select: { profileImage: true },
+    });
+
+    if (!user?.profileImage) {
+      return new NextResponse(null, { status: 404 });
+    }
+
+    // Create a response with the binary data
+    return new NextResponse(user.profileImage.data, {
+      headers: {
+        "Content-Type": user.profileImage.mimetype,
+        "Cache-Control": "public, max-age=604800", // 1 week cache
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(null, { status: 500 });
+  }
+}
