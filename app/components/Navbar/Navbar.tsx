ceedc3f9aa197436993bfa420e5f8a3d1d0991af
Joshua Ganteng <jravaellnew@gmail.com>
2025-07-24 12:44:00 +0700

    Navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
+import { AuthContext } from "@/app/providers/AuthProvider";
+import AuthenticatedMenu from "./AuthenticatedMenu";
+import Link from "next/link";
+import Reveal from "../Animations/Reveal";
+import Search from "./Search";
 
 const Navbar = () => {
-  return <div className="fixed top-0 left-0 right-0 h-24"></div>;
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Reveal direction="down">
+      <div className="nav">
+        <h1 className="font-bitcount font-normal tracking-widest text-primary-text">
+          JChatAI
+        </h1>
+        <Search />
+        {user ? (
+          <AuthenticatedMenu />
+        ) : (
+          <Link href="/register" className="btn-outline">
+            Sign Up
+          </Link>
+        )}
+      </div>
+    </Reveal>
+  );
 };
 
 export default Navbar;
